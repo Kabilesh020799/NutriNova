b0d42630031c7b79831bad5687c95c8501ddf4d6
Kabilesh <kabilesh@dal.ca>
2024-02-05 23:36:05 -0400

    CICD setup: yaml update

--- termProject/src/components/NavBar.vue
+++ termProject/src/components/NavBar.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="navbar">
-    <div class="navbar-item" v-for="(item, index) in navBAr"></div>
+    <!-- <div class="navbar-item" v-for="(item, index) in navBAr"></div> -->
   </div>
 </template>
 
 <script setup>
-import 
 
 </script>
 <style scoped lang="scss">
