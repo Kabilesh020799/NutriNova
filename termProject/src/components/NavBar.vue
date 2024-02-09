c0e894ba11ac27ce122c60a232369f6136cd3d10
Kabilesh <kabilesh@dal.ca>
2024-02-09 00:15:57 -0400

    NavBAr: avatar compoent

--- termProject/src/components/NavBar.vue
+++ termProject/src/components/NavBar.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="navbar">
     <div class="navbar-item" v-for="(item, index) in navBarConstants">{{ item.label }}</div>
-    <v-avatar color="#fff">
-      <span>KR</span>
-    </v-avatar>
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-avatar 
+          color="#fff"
+          v-bind="props"
+          style="cursor: pointer;"
+        >
+          <span>KR</span>
+        </v-avatar>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in navBarConstants"
+          :key="index"
+          :value="index"
+          class="custom-menu-item"
+        >
+          <v-list-item-title>{{ item.label }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
 </template>
 
@@ -23,5 +41,10 @@
     color: #fff;
     font-weight: bold;
     font-size: 20px;
+  }
+  .custom-menu-item:hover.v-list-item{
+    background: #8560DB !important;
+    color: #fff;
+    transition: none;
   }
 </style>
