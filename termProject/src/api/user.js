356b4a13c56449f577926be4603f65bfeba392a8
Kabilesh <kabilesh@dal.ca>
2024-04-06 11:09:28 -0300

    new changes

--- termProject/src/api/user.js
+++ termProject/src/api/user.js
@@ -81,9 +81,6 @@ const uploadLogo = async(image) => {
   let res = await fetch('', {
     method: 'POST',
     headers: {
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-      'Access-Control-Allow-Credentials' : true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
