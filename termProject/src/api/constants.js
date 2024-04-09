e8b5f08aceff8a828591129bfee2e0cf750c0e6e
Kabilesh <kabilesh@dal.ca>
2024-04-09 00:03:31 -0300

    api change

--- termProject/src/api/constants.js
+++ termProject/src/api/constants.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "http://18.233.165.43:8080/api";
+const apiEndpoint = "http://18.208.153.25:8080/api";
 const apiGateway = "https://ibk8f0qno1.execute-api.us-east-1.amazonaws.com/dev"
 
 export {
