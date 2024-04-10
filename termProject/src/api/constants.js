40559df9bb1ba5b003d579e9fd603821d7aaa0f7
Kabilesh <kabilesh@dal.ca>
2024-04-09 21:22:07 -0300

    enf

--- termProject/src/api/constants.js
+++ termProject/src/api/constants.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "http://18.208.153.25:8080/api";
+const apiEndpoint = "http://100.24.18.182/:8080/api";
 const apiGateway = "https://ibk8f0qno1.execute-api.us-east-1.amazonaws.com/dev"
 
 export {
